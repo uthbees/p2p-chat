0c124ed714dc91056c551550fe6140cb8167d032
Ethan Ball <33760679+uthbees@users.noreply.github.com>
2024-09-24 16:23:07 -0600

    Move env_args stuff into main.rs

--- src/config.rs
+++ src/config.rs
@@ -7,21 +7,10 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build_from_env_args() -> Result<Config, &'static str> {
-        let mut env_args = env::args();
-        if env_args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        // Consume the first argument, which is the program name.
-        env_args.next();
-
-        let Some(ip_arg) = env_args.next() else {
-            return Err("IP address not specified");
-        };
-
-        let parsed_ip = IpAddr::from(match ip_arg.chars().filter(|char| *char == '.').count() {
+    pub fn build(ip: &str, port: &str) -> Result<Config, &'static str> {
+        let parsed_ip = IpAddr::from(match ip.chars().filter(|char| *char == '.').count() {
             3 => {
-                let mut split_ip = ip_arg.split('.');
+                let mut split_ip = ip.split('.');
                 let mut ip_segments = [0u8; 4];
                 for elem in &mut ip_segments {
                     *elem = match split_ip
@@ -36,18 +25,14 @@ impl Config {
                 ip_segments
             }
             _ => {
-                return match ip_arg.chars().filter(|char| *char == ':').count() {
+                return match ip.chars().filter(|char| *char == ':').count() {
                     7 => Err("IPv6 is not supported"),
                     _ => Err("Invalid IP address"),
                 }
             }
         });
 
-        let Some(port_arg) = env_args.next() else {
-            return Err("Port not specified");
-        };
-
-        let Ok(parsed_port) = port_arg.parse::<u16>() else {
+        let Ok(parsed_port) = port.parse::<u16>() else {
             return Err("Invalid port");
         };
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use p2p_chat::config::Config;
-use std::process;
+use std::{env, process};
 
 fn main() {
-    let config = Config::build_from_env_args().unwrap_or_else(|err| {
+    let config = build_config_from_env_args().unwrap_or_else(|err| {
         println!("Invalid arguments: {err}");
         process::exit(1);
     });
 
     p2p_chat::run(&config);
+}
+
+fn build_config_from_env_args() -> Result<Config, &'static str> {
+    let mut env_args = env::args();
+    if env_args.len() < 3 {
+        return Err("Not enough arguments");
+    }
+    // Consume the first argument, which is the program name.
+    env_args.next();
+
+    let Some(ip_arg) = env_args.next() else {
+        return Err("IP address not specified");
+    };
+    let Some(port_arg) = env_args.next() else {
+        return Err("Port not specified");
+    };
+
+    Config::build(&ip_arg, &port_arg)
 }
