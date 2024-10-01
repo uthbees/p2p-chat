fd5acbd3bef31c6161ee8c40d9d314de59fa16ec
Ethan Ball <33760679+uthbees@users.noreply.github.com>
2024-10-01 12:35:37 -0600

    Create read_line utility function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use p2p_chat::config::Config;
-use p2p_chat::utils::print;
-use std::{io, process};
+use p2p_chat::utils::{print, read_line};
+use std::process;
 
 fn main() {
     print("Enter port to listen on: ");
 
-    let mut untrimmed_port = String::new();
-    io::stdin()
-        .read_line(&mut untrimmed_port)
-        .expect("should have been able to read line");
+    let port = read_line();
 
-    let config = Config::build("127.0.0.1", untrimmed_port.trim()).unwrap_or_else(|err| {
+    let config = Config::build("127.0.0.1", &port).unwrap_or_else(|err| {
         println!("Invalid arguments: {err}");
         process::exit(1);
     });
--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,13 @@ pub fn print(str: &str) {
     print!("{str}");
     let _ = io::stdout().flush();
 }
+
+#[must_use]
+/// A utility function to read a line of user input from the console.
+pub fn read_line() -> String {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("should have been able to read line");
+    String::from(line.trim())
+}
